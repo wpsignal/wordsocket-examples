3d97cdbf728f2cdf5fe43b02ea74f9e4861178b9
jaredrethman <jaredrethman@gmail.com>
2026-07-24 21:47:54 -0500

    test

--- test/test.php
+++ test/test.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Test
  * Description:       A test plugin for the WordSocket showcase.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            WPSignal
  * License:           GPL-2.0-or-later
  * Text Domain:       test
